52e74fc7c7ccf00cd957f74e450289a863830d8b
UnknowMilitiant <j.umariy4238@gmail.com>
2024-11-11 18:57:32 +0500

    Small change

--- apps/tests/admin.py
+++ apps/tests/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from apps.tests.models import Subject, Test, TestQuestion
+
+
 # Register your models here.
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TestQuestion)
+class TestQuestionAdmin(admin.ModelAdmin):
+    pass
